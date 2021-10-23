c10e9aec2b844a77926b00d423ad0e63eafae6ed
MatejGalic <mg51915@fer.hr>
2021-10-23 21:30:23 +0200

    Dodana funkcija za dobivanje ConnectionStringa iz DbContext-a.

--- Repositories/DatabaseContext.cs
+++ Repositories/DatabaseContext.cs
@@ -1,13 +1,26 @@
 ﻿using backend_Span_MatejGalic.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace backend_Span_MatejGalic.Repositories
 {
     public class DatabaseContext : DbContext
     {
+        private string ConnectionString { get; }
+
         public DatabaseContext(DbContextOptions<DatabaseContext> opt) : base(opt)
         {
+            var builder = new ConfigurationBuilder();
+            builder.AddJsonFile("appsettings.json", optional: false);
 
+            var configuration = builder.Build();
+
+            ConnectionString = configuration.GetConnectionString("DatabaseConnection").ToString();
+        }
+
+        public string GetConnectionString()
+        {
+            return ConnectionString;
         }
 
         public DbSet<Person> People { get; set; }
--- Repositories/SqlDatabaseRepo.cs
+++ Repositories/SqlDatabaseRepo.cs
@@ -25,7 +25,9 @@ namespace backend_Span_MatejGalic.Repositories
 
             // pozivanje procedure sa SQL servera
             // TODO: maknuti eksplicitni ConnectionString
-            using SqlConnection con = new("Data Source = localhost\\SQLEXPRESS; Initial Catalog = MatejTest; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+
+            //using SqlConnection con = new("Data Source = localhost\\SQLEXPRESS; Initial Catalog = MatejTest; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+            using SqlConnection con = new(_context.GetConnectionString());
             using SqlCommand cmd = new("InsertPerson", con);
             cmd.CommandType = CommandType.StoredProcedure;
             cmd.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = person.FirstName.Trim();
